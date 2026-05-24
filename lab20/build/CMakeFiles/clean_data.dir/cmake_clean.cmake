fe72847a2450bb3c88b970fe5b7e634f7ee65407
Leshin Artem <lesinrtm@gmail.com>
2026-05-24 15:02:00 +0700

    update repository

--- lab20/build/CMakeFiles/clean_data.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_data"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
